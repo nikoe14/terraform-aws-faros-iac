0d703d1ce4670c5c77bf4fdef675388477d543b5
Nicolas Espejo <nikoe14@gmail.com>
2020-07-22 22:56:23 -0300

    adding example

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,10 @@
 module "sample_faros" {
-  source  = "../"
+#  source  = "../"
+  source = "git::https://github.com/nikoe14/terraform-aws-faros.git"
 
   iam_role_faros_external_id = data.aws_caller_identity.current.account_id
 
-  #optional
+  # optional
   aws_iam_role_name = "faros-role"
   aws_iam_policy_name = "faros-policy"
   aws_iam_role_description = "Faros role"
